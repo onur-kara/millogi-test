828ebdf5d44548190ad4b1124176ff5ddf8abe98
onur-kara <85102414+onur-kara@users.noreply.github.com>
2024-06-20 14:07:05 +0000

    added data connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,16 @@ client = init_connection()
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def get_data():
-    db = client.mydb
-    items = db.mycollection.find()
+    db = client.Millogi
+    items = db.news_update.find()
     items = list(items)  # make hashable for st.cache_data
     return items
 
+# Fetch the data
+data = get_data()
+
+# Convert the data to a DataFrame
+df = pd.DataFrame(data)
 
 st.title("Onur's MiLoGi Tracker")
-
+st.write(df)
